3bd7accb0b8505c057533815e8e0118fedbe6157
bkre12345 <belvira@terpmail.umd.edu>
2023-06-22 16:37:58 -0400

    added homepage styling

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,12 @@
 import Footer from '../components/Footer'
+import '../Styling/Homepage.css'
 
 const Homepage = () => {
   return (
     <>
       <div className="home-container">
-        <h1 className='CodeCupid'>CodeCupid</h1>
-        <h2>connecting hearts through code</h2>
+        <h1 className='CodeCupid'>[code+cupid]</h1>
+        <h2 className='tagline'>(connecting+hearts+through code)</h2>
       </div>
 
       <Footer />
