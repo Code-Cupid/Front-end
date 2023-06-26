00db67a57add4410e5441d9b1451efa4c107a433
bkre12345 <belvira@terpmail.umd.edu>
2023-06-26 18:34:04 -0400

    made buttons look the same on homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
-import { Navbar } from "reactstrap";
+import { Navbar, NavItem } from "reactstrap";
 import { Link } from "react-router-dom";
-import '../styles/Components.css'
+import "../styles/Components.css";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <Navbar>
         <span className="copyright"> &copy;2023 Cupid </span>
-        <Link to="/cupids" className="cupid-coders">
-          About Us
-        </Link>
+        <NavItem className="nav-item">
+          <Link to="/meet-the-cupids">Meet the Cupids</Link>
+        </NavItem>
       </Navbar>
     </div>
   );
