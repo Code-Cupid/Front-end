71dd5b7a90b77d4e41e1be3fcf71087fdd054581
egonz0216 <egonz0216@gmail.com>
2023-06-27 14:39:05 -0700

    completed testing for the index page

--- src/__tests__/UserIndex.test.js
+++ src/__tests__/UserIndex.test.js
@@ -22,10 +22,54 @@ describe("<UserIndex />", () => {
       <BrowserRouter>
         <UserIndex  readmes={MockReadmes} />
       </BrowserRouter>
-      
     )
+    const heading = screen.getByRole('heading', { name: /list of users/i })
+    expect(heading).toBeInTheDocument
     screen.logTestingPlaygroundURL()
   })
 
+  it("has a name", () => {
+    render(
+      <BrowserRouter>
+        <UserIndex  readmes={MockReadmes} />
+      </BrowserRouter>
+    )
+    const name = screen.getByText(/jesse matthews/i)
+    expect(name).toBeInTheDocument
+    screen.logTestingPlaygroundURL()
+  })
+
+  it("has a age", () => {
+    render(
+      <BrowserRouter>
+        <UserIndex  readmes={MockReadmes} />
+      </BrowserRouter>
+    )
+    const age = screen.getByText(/age: 25/i)
+    expect(age).toBeInTheDocument
+    screen.logTestingPlaygroundURL()
+  })
+  
+  it("has a gender", () => {
+    render(
+      <BrowserRouter>
+        <UserIndex  readmes={MockReadmes} />
+      </BrowserRouter>
+    )
+    const gender = screen.getByText(/gender: male/i)
+    expect(gender).toBeInTheDocument
+    screen.logTestingPlaygroundURL()
+  })
+
+  it("has an image", () => {
+    render(
+      <BrowserRouter>
+        <UserIndex  readmes={MockReadmes} />
+      </BrowserRouter>
+    )
+    const image = screen.getByRole('img', { name: /apartment/i })
+    expect(image).toBeInTheDocument
+    screen.logTestingPlaygroundURL()
+  })
 
 })
